48dd2dc1162b504e1da9e0d78451ee86eb3b7f3e
Dingo337 <bbicianpv@gmail.com>
2025-01-13 13:08:49 +0100

    added functionality

--- src/pages/GameScreen.vue
+++ src/pages/GameScreen.vue
@@ -1,9 +1,10 @@
 <template>
-  <q-page class="tw-flex tw-font-Mitr tw-items-center tw-justify-center tw-relative">
+  <q-page @click="checkClickPosition" class="tw-flex tw-font-Mitr tw-items-center tw-justify-center tw-relative " 
+  :class="bg">
     <q-img src="wrong.svg" class="tw-w-16 tw-absolute tw-top-5 tw-left-5" />
     <q-img src="correct.svg" class="tw-w-16 tw-absolute tw-bottom-5 tw-left-5" />
 
-    <p class="tw-absolute tw-top-[6px] tw-left-24 tw-text-[60px] tw-text-[#ff5964]">0</p>
+    <p class="tw-absolute tw-top-[6px] tw-left-24 tw-text-[60px] tw-text-[#ff5964]">{{ right }}</p>
 
     <div class="we-flex tw-items-center tw-justify-center">
       <p
@@ -16,8 +17,8 @@
       <p
        v-if=status class=" tw-font-Mitr tw-font-semibold tw-text-6xl tw-text-center removeAfterTimeout"
       >
-        Person to Guess 
-      </p>
+      {{ currentWord }}     
+     </p>
     </div>
 
     <p
@@ -31,25 +32,59 @@
 
     </div>
 
-    <p class="tw-absolute tw-bottom-[8px] tw-left-24 tw-text-[60px] tw-text-[#048b79]">0</p>
+    <p class="tw-absolute tw-bottom-[8px] tw-left-24 tw-text-[60px] tw-text-[#048b79]">{{ wrong }}</p>
   </q-page>
 </template>
 
 <script setup>
+import guessData from "../guessList.json";
+
 import { useQuasar } from 'quasar'
 import { ref } from 'vue'
+
 const counter = ref(5)
 const status = ref(false)
+const right = ref(0)
+const wrong = ref(0)
+const bg = ref('')
+const words = ref(guessData.words)
+const currentWord = ref('') 
 
+const randomIndex = ref(Math.floor(Math.random() * words.value.length));
+currentWord.value = words.value[randomIndex.value].word; 
+
+const checkClickPosition = (e) =>{  
+    const bottomBoundary = window.innerHeight / 2;
+    
+    
+    if (e.clientY >= bottomBoundary) {
+        wrong.value++;
+        bg.value = 'tw-bg-[#048b79]';
+        setTimeout(() => {
+            bg.value = 'tw-bg-black';
+        }, 1000);
+    } else {
+        right.value++;
+        bg.value = 'tw-bg-[#ff5964]';
+        setTimeout(() => {
+            bg.value = 'tw-bg-black';
+        }, 1000);
+    }
+
+    setTimeout(() => {
+    randomIndex.value = Math.floor(Math.random() * words.value.length);
+    currentWord.value = words.value[randomIndex.value].word; 
+  }, 1000);
+}
 
 setInterval(() => {
   if (counter.value > 0) {
-    counter.value = counter.value - 1
-  }else {
-    status.value = true
+    counter.value = counter.value - 1;
+  } else {
+    status.value = true;
   }
 }, 1000)
 
-const $q = useQuasar()
-const currentPlayer = $q.localStorage.getItem('players')[0]['name']
+const $q = useQuasar();
+const currentPlayer = $q.localStorage.getItem('players')[0]['name'];
 </script>
